42a0039a8d71b224c188db2f7966b6f23ff1dad5
bizo <bilgunzorig@gmail.com>
2026-06-05 14:42:33 +0800

    added word mike tyson on 14th line

--- app.py
+++ app.py
@@ -10,6 +10,6 @@ if __name__ == "__main__":
     # host="0.0.0.0" opens up the server so ngrok can actually see ita
     app.run(host="0.0.0.0", port=5000, debug=True)
 
-             
 
-hey
+    miketyson
+    
